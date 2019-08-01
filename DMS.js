7570c201acc7fcb6e8d3cd5369023d93bf47528a
이성진 <chlskatnr1@naver.com>
2019-08-01 13:59:59 +0900

    [Fimal] DMS End
    DMS 끝

--- DMS.js
+++ DMS.js
@@ -24,12 +24,14 @@ function mouseLeave(num) {
     })
 }
 const mainApply = document.getElementsByClassName('main-apply');
-var bgImg = document.getElementById(`apply-bg-img$1`);
-var a = bgImg.getAttribute()
-function test(index) {
-    mainApply[index].addEventListener('mouseenter', mouseEnter(index));
-    mainApply[index].addEventListener('mouseleave', mouseLeave(index));
+for(var i = 0; i < mainApply.length; i++) {
+    mouseEnter(i);
+    mouseLeave(i);
 }
+// function test(index) {
+//     mainApply[index].addEventListener('mouseenter', mouseEnter(index));
+//     mainApply[index].addEventListener('mouseleave', mouseLeave(index));
+// }
 // mainApply[0].addEventListener('mouseenter', mouseEnter(0));
 // mainApply[0].addEventListener('mouseleave', mouseLeave(0));
 // mainApply[1].addEventListener('mouseenter', mouseEnter(1));
